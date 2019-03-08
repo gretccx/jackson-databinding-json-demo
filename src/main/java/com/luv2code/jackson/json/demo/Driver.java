f72aed92b2675f4136d45dfa102ce819c8d489c1
Gret <gretccx@gmail.com>
2019-03-07 19:12:26 -0600

    Added more JSON info.

--- src/main/java/com/luv2code/jackson/json/demo/Driver.java
+++ src/main/java/com/luv2code/jackson/json/demo/Driver.java
@@ -14,11 +14,21 @@ public class Driver {
 			
 			// read JSON file and map/convert to java POJO: /data/sample-lite.json
 			Student theStudent =
-					mapper.readValue(new File("data/sample-lite.json"), Student.class);
+					mapper.readValue(new File("data/sample-full.json"), Student.class);
 			
 			// print first name and last name
 			System.out.println("First name = " + theStudent.getFirstName());
 			System.out.println("Last name = " + theStudent.getLastName());
+			
+			// print out address: street and city
+			Address tempAddress = theStudent.getAddress();
+			System.out.println("Street = " + tempAddress.getStreet());
+			System.out.println("City = " + tempAddress.getCity());
+			
+			// print out languages
+			for (String tempLang : theStudent.getLanguages()) {
+				System.out.println(tempLang);
+			}
 			
 		} catch (Exception e) {
 			e.printStackTrace();
